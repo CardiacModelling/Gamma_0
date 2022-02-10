d47c7cf313d02c36f079d5513c44eeb14b5d9c28
Michael Clerx <michael.clerx@nottingham.ac.uk>
2022-02-10 22:39:42 +0000

    Fixed x axis in fig 2

--- Scripts/Fig 2 - Solver tolerance.py
+++ Scripts/Fig 2 - Solver tolerance.py
@@ -8,16 +8,19 @@ Created on Tue Jan 21 14:25:17 2020
 
 import sabs_pkpd
 import matplotlib.pyplot as plt
-import os 
+import os
+import numpy as np
 
 # Select the folder in which this repo is downloaded in the line below
-os.chdir('The/location/of/the/root/folder/of/this/repo')
-    
+os.chdir('../')
+
 
 # In[Load model]
 filename = './Models/OHara CiPA.mmt'
 s = sabs_pkpd.load_model.load_simulation_from_mmt(filename)
 
+runtime = 2000000
+log_interval = 1000
 
 # In[Select initial conditions]
 # Names of the state variables in the MMT model
@@ -28,7 +31,7 @@ s.set_tolerance(abs_tol = 1e-06, rel_tol = 1e-04)
 # Reset the model to the initial conditions retrieved when loading the model
 s.set_state(sabs_pkpd.constants.default_state)
 # run the model to limit cycle
-a = s.run(2000000, log_interval = 1000)
+a = s.run(runtime, log_interval = log_interval)
 
 
 # Set the solver tolerance to low
@@ -36,15 +39,20 @@ s.set_tolerance(abs_tol = 1e-08, rel_tol = 1e-06)
 # Reset the model to the initial conditions retrieved when loading the model
 s.set_state(sabs_pkpd.constants.default_state)
 # run the model to limit cycle
-a1 = s.run(2000000, log_interval = 1000)
+a1 = s.run(runtime, log_interval = log_interval)
 
 
 # In[Plot the results]
 state_name = 'membrane.V'
 
+offset = 250
+y = a[state_name][offset:]
+y1 = a1[state_name][offset:]
+x = np.arange(offset, offset + len(y))
+
 plt.figure(figsize = (7.5, 7.5))
-plt.plot(a[state_name][250:], label = 'abs_tol = 1e-06, rel_tol = 1e-04', linewidth = 5)
-plt.plot(a1[state_name][250:], label = 'abs_tol = 1e-08, rel_tol = 1e-06', linewidth = 5)
+plt.plot(x, y, label = 'abs_tol = 1e-06, rel_tol = 1e-04', linewidth = 5)
+plt.plot(x, y1, label = 'abs_tol = 1e-08, rel_tol = 1e-06', linewidth = 5)
 plt.xlabel('# of paces', fontsize = 15)
 plt.ylabel('Resting membrane potential (mV)', fontsize = 15)
 plt.xticks(fontsize = 15)
@@ -53,5 +61,4 @@ plt.legend(fontsize = 15)
 # Save
 plt.tight_layout()
 plt.savefig('./Figures/Numerical error.png')
-
 
