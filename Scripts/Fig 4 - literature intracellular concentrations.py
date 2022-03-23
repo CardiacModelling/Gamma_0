5ba091ee16a4b1dab58c8f530474c797f645828b
YannStanislasBarral <56925303+YannStanislasBarral@users.noreply.github.com>
2022-03-23 16:17:54 +0100

    Update Fig 4 - literature intracellular concentrations.py

--- Scripts/Fig 4 - literature intracellular concentrations.py
+++ Scripts/Fig 4 - literature intracellular concentrations.py
@@ -65,8 +65,9 @@ plt.vlines(x = 4, ymin = 120, ymax = 152.4, linestyle = '--', color = 'k', linew
 plt.vlines(x = 16, ymin = 120, ymax = 152.4, linestyle = '--', color = 'k', linewidth = 4)
 
 # Label Tomek and Grandi models
-plt.text(x= 8.6, y = 117, s = 'Grandi et al. 2010', fontsize = 21)
-plt.text(x= 13.7, y = 149, s = 'Tomek et al. 2020', fontsize = 21)
+plt.text(x= 4.1, y = 113, s = 'Grandi et al. 2010', fontsize = 21)
+plt.text(x= 13.7, y = 156, s = 'Tomek et al. 2020', fontsize = 21)
+plt.ylim(95, 160)
 
 # Add the legend
 plt.scatter([], [], color = 'k', marker = 'o', s = 200, label = 'Ventricle')
@@ -79,43 +80,3 @@ plt.legend(fontsize = 22, loc='lower right')
 plt.tight_layout()
 plt.savefig('./Figures/Range of concentrations.png', dpi = 300)
 
-"""
-plt.subplot(1, 2, 2)
-plt.title('At limit cycle', Fontsize = 30)
-plt.xlabel('$[Na^+]_i$ (mM)', Fontsize = size_labels)
-plt.ylabel('$[K^+]_i$ (mM)', Fontsize = size_labels)
-plt.xlim([4, 19])
-plt.ylim([50, 180])
-plt.xticks([4, 6, 8, 10, 12, 14, 16, 18], [4, 6, 8, 10, 12, 14, 16, 18], Fontsize = size_labels)
-plt.yticks(Fontsize = size_labels)
-for row in range(data['Model'].count()-4):
-    if data['Species'][row] == 'Human':
-        color = 'r'
-    if data['Species'][row] == 'Dog':
-        color = 'darkgreen'
-    if data['Species'][row] == 'Rabbit':
-        color = 'darkblue'
-    if data['Species'][row] == 'GuineaPig':
-        color = 'purple'
-    if data['Species'][row] == 'Mammalian':
-        color = 'orange'
-    if data['Species'][row] == 'Rat':
-        color = 'grey'
-    if data['Species'][row] == 'Mouse':
-        color = 'grey'
-        
-    
-    if data['Tissue'][row] == 'Ventricle':
-        shape = 'o'
-    if data['Tissue'][row] == 'Atrium':
-        shape = '+'
-    if data['Tissue'][row] == 'Purkinje':
-        shape = 'v'
-    if data['Tissue'][row] == 'SAN':
-        shape = 'x'  
-     
-    plt.scatter(data['Sodium_i_steady'][row], data['Potassium_i_steady'][row], color = color, marker = shape, s = 200)
-
-plt.tight_layout()
-
-"""
